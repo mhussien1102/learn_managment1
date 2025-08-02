2ef72c0787ebd5d0382bb74299b3687d344ab511
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-08-02 19:46:37 +0300

    added product model

---
+++ lib/core/model/product_model.dart
@@ -0,0 +1,29 @@
+class ProductModel {
+  final String id;
+  final String name;
+  final String desc;
+  final String brand;
+  final String category;
+  final String type;
+  final String disease;
+  final String sideEffect;
+  final int price;
+  final int maxQuantity;
+  final int sales;
+  final String url;
+
+  ProductModel({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.brand,
+    required this.category,
+    required this.type,
+    required this.disease,
+    required this.sideEffect,
+    required this.price,
+    required this.maxQuantity,
+    required this.sales,
+    required this.url,
+  });
+}
