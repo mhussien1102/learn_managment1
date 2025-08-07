21afec69e9eaf4f055f42679c16b85dc166d459a
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2025-08-07 23:28:03 +0300

    finish the UI spacing

---
+++ lib/feature/ecommerce/widgets/ui_spacing.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+
+class UISpacing {
+  static const gap4 = SizedBox(height: 4);
+  static const gap8 = SizedBox(height: 8);
+  static const gap12 = SizedBox(height: 12);
+  static const gap16 = SizedBox(height: 16);
+  static const gap24 = SizedBox(height: 24);
+
+  static const pad = 16.0;
+}
